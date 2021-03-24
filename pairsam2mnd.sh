3473ab2eec4b6a75a028f3a5f8c5781958ecad94
Remi-Andre Olsen <remi-andre.olsen@scilifelab.se>
2021-03-24 15:30:52 +0100

    Harmonizing threads argument

--- pairsam2mnd.sh
+++ pairsam2mnd.sh
@@ -3,13 +3,13 @@
 usage() { echo "Usage: $0 -i <pairsam.gz> -o <mndfile.txt>" 1>&2; exit 1; }
 
 dname=`dirname "$0"`
-pigz_t=16
-py_t=16
+threads=16
 
 while getopts ":i:o:" opt; do
   case ${opt} in
     i) infile=${OPTARG} ;;
     o) outfile=${OPTARG} ;;
+    t) threads=${OPTARG} ;;
     *) usage;;
   esac
 done
@@ -20,10 +20,10 @@ if [ -z "${infile}" ] || [ -z "${outfile}" ]; then
 fi
 
 if hash parallel 2>/dev/null; then
-    pigz -p $pigz_t -c -d $infile > tmp.pairsam
-    echo parallel -j $py_t --block -1 --pipepart -a tmp.pairsam $dname/get_mndfields.py | \
-    sort -m -k2,2d -k6,6d --parallel=$sort_t > $outfile
-    #rm tmp.pairsam
+    pigz -p $threads -c -d $infile > tmp.pairsam
+    parallel -j $threads --block -1 --pipepart -a tmp.pairsam $dname/get_mndfields.py | \
+    sort -m -k2,2d -k6,6d --parallel=$threads > $outfile
+    rm tmp.pairsam
 else
     echo "Error: gnu parallel not found"
 fi
